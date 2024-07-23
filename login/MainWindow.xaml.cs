a3f481a821dfce27846f070bd5177c8329e1ef5e
damidevel0per <damidevtech@gmail.com>
2024-07-23 19:53:02 -0300

    ..

---
+++ login/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace login
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string user = txtUser.Text;
+            string password = txtPassword.Text;
+
+            if (user == "admin" && password == 1234.ToString())
+            {
+                MessageBox.Show("Acceso exitoso");
+            }
+            else MessageBox.Show("Usuario o contraseña invalido");
+        
+        }
+
+        private void txtPassword_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnminus_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void btnclose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
